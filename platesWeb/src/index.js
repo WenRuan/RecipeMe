0d38023353a3bba4feb2c4e24ddaf23f355ff261
Wen Ruan <slimpandaw@gmail.com>
2021-09-22 00:09:46 -0700

    Added login test

--- platesWeb/src/index.js
+++ platesWeb/src/index.js
@@ -1,7 +1,25 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+class LoginForm extends React.Component {
+  render() {
+    return(
+      <div className="LoginForm">
+        <h1>Hello World</h1>
+        <form>
+          <label for="fname">First Name:</label>
+          <input type="text" id="fname" name="fname"/><br/><br/>
+          <label for="lname">Last Name:</label>
+          <input type="text" id="lname" name="lname"/><br/><br/>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, Deez Nutz!</h1>,
+  <LoginForm/>,
   document.getElementById('root')
 );
