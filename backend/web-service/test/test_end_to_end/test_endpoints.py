0f67fa8b8de07267beffd646896a58dcbf505cab
bjrichards <braedenrichards@nevada.unr.edu>
2021-09-29 00:54:08 -0700

    Added new end-to-end tests for create-user, query-user, login, and added supporting fixtures.

--- backend/web-service/test/test_end_to_end/test_endpoints.py
+++ backend/web-service/test/test_end_to_end/test_endpoints.py
@@ -17,10 +17,28 @@ def test_blueprint_api_test_post(api_v1_host):
     assert 'msg' in json
     assert json['msg'] == "Test POST endpoint from blueprint_api is successful."
 
-def test_blueprint_api_create_user_post(api_v1_host):
+def test_blueprint_api_login(api_v1_host, py_admin_user):
+    login_endpoint = os.path.join(api_v1_host, 'api', 'login').replace("\\", "/")
+    
+    # Login
+    response_login = requests.post(login_endpoint, data={}, auth=py_admin_user)
+    assert response_login.status_code == 200
+    response_token = response_login.json()
+    assert "token" in response_token
+
+def test_blueprint_api_create_user_post(api_v1_host, test_user_admin, py_test_admin_token):
     endpoint = os.path.join(api_v1_host, 'api', 'create-user').replace("\\", "/")
-    response = requests.post(endpoint)
+    # Create user
+    response = requests.post(endpoint, data=test_user_admin, headers=py_test_admin_token)
     assert response.status_code == 200
     json = response.json()
     assert 'msg' in json
-    assert json['msg'] == "User creation request accepted."
+    assert json['msg'] == "Create-user POST endpoint from blueprint_api is successful."
+
+def test_blueprint_api_query_single_user(api_v1_host, py_test_user, py_test_admin_token):
+    endpoint = os.path.join(api_v1_host, 'api', 'query-user', '19').replace("\\", "/")
+    # Query User
+    response = requests.getresponse = requests.get(endpoint, headers=py_test_admin_token)
+    assert response.status_code == 200
+    json = response.json()
+    assert json == py_test_user
